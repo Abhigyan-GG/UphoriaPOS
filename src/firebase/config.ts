ebd05962d32851c6d17f3c567c37c95717edd165
abhigyan manit jha <manitjha032@gmail.com>
2026-02-12 18:42:17 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,11 @@
 import { initializeApp, getApp, getApps, type FirebaseOptions } from 'firebase/app';
 
 const firebaseConfig: FirebaseOptions = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  projectId: "studio-3553753010-92c77",
+  appId: "1:1000254178969:web:138a8dca0b02f2ab645a08",
+  apiKey: "AIzaSyDAxbC4dOKO3FhRBp-SJ2_XOFmf5X_lJT4",
+  authDomain: "studio-3553753010-92c77.firebaseapp.com",
+  messagingSenderId: "1000254178969"
 };
 
 function initializeFirebase() {
